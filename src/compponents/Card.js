6baff26543e1530accbcdb78a415e3a589e18aec
Shweta Kanekar <kanekarshweta10@gmail.com>
2022-12-02 23:16:53 +0530

    Added styles.

--- src/compponents/Card.js
+++ src/compponents/Card.js
@@ -1,15 +1,8 @@
 function Card(props) {
   return (
     <div className="github-profile">
-      <img
-        src={props.avatar_url}
-        alt=""
-        style={{ width: '75px', height: '75px', color: '#ccc' }}
-      />
-      <div
-        className="info"
-        style={{ display: 'inline-block', marginLeft: '10px' }}
-      >
+      <img src={props.avatar_url} alt={props.login} />
+      <div className="info">
         <div className="name">{props.name}</div>
         <div className="username">@{props.login}</div>
         <div className="location">{props.location}</div>
--- src/compponents/CardList.js
+++ src/compponents/CardList.js
@@ -1,9 +1,13 @@
 import Card from './Card';
 
 function CardList(props) {
-  return props.profiles.map((profile) => (
-    <Card key={profile.id} {...profile} />
-  ));
+  return (
+    <div className="card-list">
+      {props.profiles.map((profile) => (
+        <Card key={profile.id} {...profile} />
+      ))}
+    </div>
+  );
 }
 
 export default CardList;
